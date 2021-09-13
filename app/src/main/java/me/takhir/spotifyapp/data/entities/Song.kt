242f0ef02a865975474f770c9139706c17a594ff
takhir <takhir.004@gmail.com>
2021-09-13 14:15:23 +0600

    Initial commit

---
+++ app/src/main/java/me/takhir/spotifyapp/data/entities/Song.kt
@@ -0,0 +1,10 @@
+package me.takhir.spotifyapp.data.entities
+
+
+data class Song(
+    val mediaId: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val imageUrl: String = "",
+    val songUrl: String = ""
+)
---
+++ app/src/main/java/me/takhir/spotifyapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package me.takhir.spotifyapp.util
+
+
+object Constants  {
+
+    const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
+}
